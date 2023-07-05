30dca5011365f53872a5b14bf335c3b49b377933
Marijndh <marijn.den.hollander@student.nhlstenden.com>
2023-07-05 19:19:35 +0200

    PDF Readable

--- main.py
+++ main.py
@@ -1,44 +1,77 @@
 import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import win32com.client
-
-os.system('cls')
+import pyautogui
+import pyperclip
+from pdfminer.high_level import extract_text
 
 
 # Find email based on input
 # https://towardsdatascience.com/automatic-download-email-attachment-with-python-4aa59bc66c25
-def VindEmail():
-    input_subject = input("Wat is de titel van de email?:  ")
-
+def FindEmail(titel):
+    message = None
+    result = None
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     inbox = outlook.GetDefaultFolder(6)
     messages = inbox.Items
     for msg in messages:
-        try:
-            if msg.subject.lower() == input_subject.lower():
-                message = msg
-                break
-            else:
-                message = None
-        except Exception:
-            print(Exception)
-    if message == None:
+        if msg.subject.lower() == titel.lower():
+            message = msg
+            break
+    if message is None:
         os.system('cls')
-        print("Email niet gevonden, probeer opnieuw")
-        VindEmail()
+        print("Email niet gevonden")
+        titel = input("Wat is de titel van de email?: ")
+        FindEmail(titel)
     else:
         print("Gevonden")
-        return message.Body
+        for attachment in message.Attachments:
+            if str(attachment)[-3:] == 'pdf':
+                result = attachment
+                break
+        result.SaveAsFile(os.path.join(__file__[0:-7], 'file.pdf'))
 
 
-text = VindEmail()
+def GetTitel():
+    hm = pyHook.HookManager()
+    hm.MouseAll = uMad
+    hm.KeyAll = uMad
+    hm.HookMouse()
+    hm.HookKeyboard()
+    pythoncom.PumpMessages()
+    first = pyautogui.position()
+    loc = pyautogui.locateCenterOnScreen("leftOftitel.png")
+    if loc is None:
+        print("Open eerst outlook voordat het programma werkt, of geef de titel door")
+        return ''
+    pyautogui.moveTo(loc.x + 20, loc.y - 2)
+    pyautogui.moveTo(loc.x + 1200, loc.y - 2)
+    pyautogui.dragTo(loc.x, loc.y)
+    pyautogui.hotkey('ctrl', 'c')
+    title = pyperclip.paste()
+    pyautogui.leftClick()
+    pyautogui.moveTo(first)
+    return title
 
-# open website
-driver = webdriver.Chrome()
-driver.get("https://www.google.com")
 
-# send data to html elements
-driver.find_element(By.ID, "L2AGLb").click()
-element = driver.find_element(By.ID, "APjFqb")
-element.send_keys("Hallo")
+def InsertData(data):
+    # open website
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+
+    # send data to html elements
+    driver.find_element(By.ID, "L2AGLb").click()
+    element = driver.find_element(By.ID, "APjFqb")
+    element.send_keys("Hallo")
+
+
+def FilterData():
+    _pdf = "file.pdf"
+    _dataRows = extract_text(_pdf)
+    print(_dataRows)
+
+
+os.system('cls')
+FilterData()
