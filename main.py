af58c70e55a2e81955fb738b9404429b51765e73
Marijndh <marijn.den.hollander@student.nhlstenden.com>
2023-07-26 12:28:24 +0200

    1.0.0

--- main.py
+++ main.py
@@ -1,69 +1,79 @@
-import xmlrpc.client
+import datetime
+import re
+import ssl
 from os import system, path
 from time import sleep
-from selenium import webdriver
-from selenium.webdriver import ActionChains, Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-import win32com.client
-import pyautogui
-from pyperclip import paste
-from pdfminer.high_level import extract_text
-from nltk import word_tokenize
-import pandas as pd
+from sys import exit
+
 import certifi
-import ssl
 import geopy.geocoders
+import pandas as pd
+import win32com.client
 from geopy.geocoders import Nominatim
+from nltk import word_tokenize
+from pdfminer.high_level import extract_text
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
 ctx = ssl.create_default_context(cafile=certifi.where())
 geopy.geocoders.options.default_ssl_context = ctx
 
 
-# Find email based on input
-# https://towardsdatascience.com/automatic-download-email-attachment-with-python-4aa59bc66c25
+def ChooseAndSetPDF():
+    for x in range(len(results)):
+        print(results[x].Filename + '  (' + str(x) + ')')
+    getal = input()
+    try:
+        results[int(getal)].SaveAsFile(path.join(__file__[0:-7], 'file.pdf'))
+        del results[int(getal)]
+    except:
+        system('cls')
+        print('Verkeerd getal \n We proberen het opnieuw')
+
+
+def ReadAttachments(message):
+    global results
+    for attachment in message.Attachments:
+        if str(attachment)[-3:] == 'pdf':
+            results.append(attachment)
+    if len(results) == 1:
+        results[0].SaveAsFile(path.join(__file__[0:-7], 'file.pdf'))
+    elif len(results) > 1:
+        system('cls')
+        print('Welke pdf wil je uit laten lezen? \n Geef het getal wat achter de pdf staat')
+        ChooseAndSetPDF()
+    else:
+        print("Geen pdf te vinden in deze mail \n we proberen het opnieuw")
+        FindEmail(None)
+
+
 def FindEmail(titel):
     message = None
-    result = None
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
     inbox = outlook.GetDefaultFolder(6)
     messages = inbox.Items
-    for msg in messages:
-        if msg.subject.lower() == titel.lower():
-            message = msg
-            break
+    outlook = win32com.client.Dispatch("Outlook.Application")
+    if outlook.ActiveExplorer() is not None:
+        selection = outlook.ActiveExplorer().Selection
+        if selection.Count > 0:
+            message = selection.Item(1)
+    if titel is not None:
+        for msg in messages:
+            if msg.subject.lower() == titel.lower():
+                message = msg
+                break
     if message is None:
         system('cls')
-        print("Email niet gevonden")
         titel = input("Wat is de titel van de email?: ")
         FindEmail(titel)
     else:
-        print("Gevonden")
-        for attachment in message.Attachments:
-            if str(attachment)[-3:] == 'pdf':
-                result = attachment
-                break
-        result.SaveAsFile(path.join(__file__[0:-7], 'file.pdf'))
-
-
-def GetTitel():
-    first = pyautogui.position()
-    loc = pyautogui.locateCenterOnScreen("leftOftitel.png")
-    if loc is None:
-        print("Open eerst outlook voordat het programma werkt, of geef de titel door")
-        return ''
-    pyautogui.moveTo(loc.x + 20, loc.y - 2)
-    pyautogui.moveTo(loc.x + 1200, loc.y - 2)
-    pyautogui.dragTo(loc.x, loc.y)
-    pyautogui.hotkey('ctrl', 'c')
-    title = paste()
-    pyautogui.leftClick()
-    pyautogui.moveTo(first)
-    return title
+        ReadAttachments(message)
 
 
 def FilterData():
@@ -71,28 +81,15 @@ def FilterData():
     try:
         _dataRows = extract_text(_pdf)
     except:
-        x = 10
-        print("PDF bestand is niet mogelijk om te openen\nProbeer het opnieuw\nAan het afsluiten...")
-        while x > 0:
-            x -= 1
-            if x != 0:
-                print(str(x), end='')
-                sleep(0.2)
-                print('.', end='')
-                sleep(0.2)
-                print('.', end='')
-                sleep(0.2)
-                print('.', end='')
-                sleep(0.4)
-            else:
-                print('0')
-                sleep(1)
+        system('cls')
+        print("PDF bestand is niet mogelijk om te openen\nProbeer het opnieuw\n")
         exit()
 
     return word_tokenize(_dataRows)
 
 
 def ClientNotFound():
+    system('cls')
     c_input = input("Opdrachtgever kan niet gevonden worden, wie is de opdrachtgever?\nBW of WSAP?: ")
     c_input_upper = c_input.upper()
 
@@ -140,49 +137,74 @@ class FillSwitch:
         for w in range(len(w_list)):
             if w_list[w] == 'Opdrachtnummer':
                 return w_list[w + 2]
-        return None
+        return ''
 
     def BW_Projectnaam(self):
+        straat = ''
+        huisnummer = ''
+        woonplaats = ''
         for w in range(len(w_list)):
             if w_list[w] == 'Gereed' and w_list[w + 1] == 'voor':
-                return 'Test: BW ' + w_list[w + 4] + ' ' + w_list[w + 5] + ' ' + w_list[w + 9]
-        return None
+                for z in range(len(w_list) - w):
+                    if w_list[w + z].isnumeric() and straat == '':
+                        straat = ' '.join(w_list[w + 4:w + z])
+                        huisnummer = w_list[w + z]
+                    if re.search(r"\b\d{4}\b", w_list[w + z]) and re.search(r"\b[A-Z]{2}\b", w_list[w + z + 1]):
+                        woonplaats = w_list[w + z + 2]
+
+        return 'BW ' + straat + ' ' + huisnummer + ' ' + woonplaats
 
     def BW_Postcode(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Gereed' and w_list[w + 1] == 'voor':
-                return w_list[w + 7] + ' ' + w_list[w + 8]
-        return None
+                for z in range(len(w_list) - w):
+                    if re.search(r"\b\d{4}\b", w_list[w + z]) and re.search(r"\b[A-Z]{2}\b", w_list[w + z + 1]):
+                        return w_list[w + z] + ' ' + w_list[w + z + 1]
+        return ''
 
     def BW_Huisnummer(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Gereed' and w_list[w + 1] == 'voor':
-                return w_list[w + 5]
-        return None
+                for z in range(len(w_list)):
+                    if w_list[w + z].isnumeric():
+                        return w_list[w + z]
+        return ''
 
     def BW_Straat(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Gereed' and w_list[w + 1] == 'voor':
-                return w_list[w + 4]
-        return None
+                for z in range(0, w + 2):
+                    if w_list[w + z].isnumeric():
+                        return ' '.join(w_list[w + 4:w + z])
+        return ''
 
     def BW_Plaats(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Gereed' and w_list[w + 1] == 'voor':
-                return w_list[w + 9]
-        return None
+                for z in range(len(w_list) - w):
+                    if re.search(r"\b\d{4}\b", w_list[w + z]) and re.search(r"\b[A-Z]{2}\b", w_list[w + z + 1]):
+                        return w_list[w + z + 2]
+        return ''
 
     def BW_Telefoon(self):
+        number = ''
         for w in range(len(w_list)):
-            if w_list[w] == 'Gereed' and w_list[w + 1] == 'voor':
-                return w_list[w + 18]
-        return None
+            if w_list[w][0] == '0' and w_list[w][1] == '6':
+                if len(w_list[w]) >= 10:
+                    return w_list[w]
+                else:
+                    for x in range(9):
+                        if w_list[w + x].isnumeric():
+                            number += w_list[w + x]
+                        else:
+                            return number
+        return ''
 
     def BW_Email(self):
         for w in range(len(w_list)):
             if w_list[w] == '@':
                 return w_list[w - 1] + w_list[w] + w_list[w + 1]
-        return None
+        return ''
 
     def BW_Contact(self):
         return "Woningbouwvereniging Beter Wonen"
@@ -210,75 +232,96 @@ class FillSwitch:
                     else:
                         om += omschrijving[dots[d]]
                 return om
-        return None
+        return ''
 
     def WSAP_Referentie(self):
         for w in range(len(w_list)):
             if w_list[w] == '/' and w_list[w + 2] == '/':
                 return w_list[w - 1] + ' ' + w_list[w] + ' ' + w_list[w + 1] + ' ' + w_list[w + 2] + ' ' + w_list[w + 3]
-        return None
+        return ''
 
     def WSAP_Projectnaam(self):
-        projectname = 'Test: WSAP '
+        straat = ''
+        woonplaats = ''
+        geolocator = Nominatim(user_agent="geoapiExercises")
         for w in range(len(w_list)):
             if w_list[w] == 'Object' and w_list[w + 1] == ':':
-                for x in range(len(w_list) - w):
-                    if w_list[w + 2 + x] != ',':
-                        projectname += w_list[w + 2 + x] + ' '
-                    else:
-                        projectname += w_list[w + 2 + x + 1]
+                for z in range(len(w_list)):
+                    if w_list[w + z].isnumeric():
+                        straat = ' '.join(w_list[w + 2:w + z + 1])
                         break
-                return projectname
-        return None
+        try:
+            loc = geolocator.geocode(straat)
+            woonplaats = geolocator.reverse(loc.point).raw['address']['town']
+        except:
+            print('')
+
+        return 'WSAP ' + straat + ' ' + woonplaats
 
     def WSAP_Postcode(self):
         geolocator = Nominatim(user_agent="geoapiExercises")
         adress = ''
         for w in range(len(w_list)):
             if w_list[w] == 'Object' and w_list[w + 1] == ':':
-                for x in range(len(w_list) - w):
-                    if w_list[w + 2 + x] != ',':
-                        adress += w_list[w + 2 + x] + ' '
-                    else:
-                        adress += w_list[w + 2 + x + 1]
+                for z in range(len(w_list)):
+                    if w_list[w + z].isnumeric():
+                        adress = ' '.join(w_list[w + 2:w + z + 1])
                         break
-                return ' '.join(geolocator.geocode(adress).raw['display_name'].split()[-3:-1])[:-1]
-        return None
+        try:
+            loc = geolocator.geocode(adress)
+            return geolocator.reverse(loc.point).raw['address']['postcode']
+        except:
+            return ''
 
     def WSAP_Huisnummer(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Object' and w_list[w + 1] == ':':
-                for x in range(len(w_list) - w):
-                    if w_list[w + x] == ',':
-                        return w_list[w + x - 1]
-        return None
+                for z in range(len(w_list)):
+                    if w_list[w + z].isnumeric():
+                        return w_list[w + z]
+        return ''
 
     def WSAP_Straat(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Object' and w_list[w + 1] == ':':
-                for x in range(len(w_list) - w):
-                    if w_list[w + 2 + x] == ',':
-                        return ' '.join(w_list[w + 2: w + 1 + x])
-        return None
+                for z in range(len(w_list)):
+                    if w_list[w + z].isnumeric():
+                        return ' '.join(w_list[w + 2:w + z])
+        return ''
 
     def WSAP_Plaats(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Object' and w_list[w + 1] == ':':
-                for x in range(len(w_list) - w):
+                for x in range(len(w_list) - w - 2):
                     if w_list[w + 2 + x] == ',':
                         return w_list[w + 3 + x]
-        return None
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        adress = ''
+        for w in range(len(w_list)):
+            if w_list[w] == 'Object' and w_list[w + 1] == ':':
+                for z in range(len(w_list)):
+                    if w_list[w + z].isnumeric():
+                        adress = ' '.join(w_list[w + 2:w + z + 1])
+                        break
+        try:
+            loc = geolocator.geocode(adress)
+            return geolocator.reverse(loc.point).raw['address']['town']
+        except:
+            return ''
 
     def WSAP_Telefoon(self):
         for w in range(len(w_list)):
             if w_list[w] == 'Telefoonnummer' and w_list[w + 1] == ':':
-                for x in range(len(w_list) - w):
+                for x in range(len(w_list) - w - 2):
                     if w_list[w + 2 + x] == ':' and w_list[w + 3 + x] == '06':
                         return ''.join(w_list[w + 3 + x:w + 2 + x + 6])
-        return None
+        return ''
 
     def WSAP_Email(self):
-        return None
+        for w in range(len(w_list)):
+            if w_list[w] == '@':
+                return w_list[w - 1] + w_list[w] + w_list[w + 1]
+        return ''
 
     def WSAP_Contact(self):
         return 'Woningstichting Anna Paulowna'
@@ -296,7 +339,7 @@ class FillSwitch:
                     opdracht += w_list[w + 2 + x]
                     if w_list[w + 3 + x] != '.':
                         opdracht += ' '
-        return None
+        return ''
 
 
 def InsertDataIntoInputs():
@@ -360,16 +403,15 @@ def InsertData(driver):
                         break
 
         except Exception as e:
-            print(str(e))
-            print('\n' + inputitem.name)
+            print('Error bij het invullen van: ' + inputitem.name)
 
 
 def StartupDriver():
     options = Options()
     options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    service = Service(ChromeDriverManager(version="114.0.5735.90").install())
+    driver = webdriver.Chrome(options=options, service=service)
     driver.maximize_window()
-    driver.set_window_position(0, -1200)
     driver.get("https://login.bouw7.nl/")
     try:
         mailinput = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "loginform-username")))
@@ -392,15 +434,81 @@ def StartupDriver():
     except:
         driver.quit()
         print("Kan de juiste website elementen niet vinden om naar de website te gaan, probeer opnieuw")
+        exit()
     InsertData(driver)
     elementpos = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div/div[1]/h1")
     ActionChains(driver).move_to_element(elementpos).click(elementpos).perform()
-    driver.set_window_position(0, 0)
 
 
-system('cls')
-inputvalue_list = CreateInputValues()
-w_list = FilterData()
-c = GetClient()
-InsertDataIntoInputs()
-StartupDriver()
+def Again():
+    global c
+    global w_list
+    global inputvalue_list
+    system('cls')
+    if len(results) > 0:
+        again = input('Wil je nog een pdf laten invoeren uit dezelfde mail?: (Y/N) \n')
+        if again.upper() == 'Y':
+            ChooseAndSetPDF()
+    again = input('Wil je nog een pdf laten invoeren uit een nieuwe mail?: (Y/N) \n')
+    if again.upper() == 'Y':
+        print('Druk op enter als de volgende mail klaar staat')
+        input('')
+        sleep(1)
+        Main()
+    if again.upper() == 'N':
+        exit()
+    else:
+        system('cls')
+        print('Verkeerde input, voer Y of N in als YES of NO')
+        Again()
+
+
+def EverythingWhentWell():
+    system('cls')
+    i = input('Is alles goed gegaan?: (Y/N) \n')
+    if i.upper() == 'Y':
+        Again()
+    if i.upper() == 'N':
+        file = open('log.txt', 'w')
+        data = input('Wat is er precies fout gegaan?: \n')
+        if data != '':
+            time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+            file.write(time + ': ' + data + '\n')
+            for x in inputvalue_list:
+                if type(x.value) is list:
+                    file.write('[' + x.name + ': ')
+                    for y in range(len(x.value)):
+                        file.write(x.value[y] + ' ')
+                    file.write('] ')
+                else:
+                    file.write('[' + x.name + ': ' + str(x.value) + '] ')
+            file.write('\n\n')
+            Again()
+        else:
+            print('Er is wat fout gegaan, we proberen het opnieuw')
+            EverythingWhentWell()
+    else:
+        system('cls')
+        print('Verkeerde input, voer Y of N in als YES of NO')
+        EverythingWhentWell()
+
+
+def Main():
+    global c
+    global w_list
+    global inputvalue_list
+    system('cls')
+    FindEmail(None)
+    inputvalue_list = CreateInputValues()
+    w_list = FilterData()
+    c = GetClient()
+    InsertDataIntoInputs()
+    StartupDriver()
+    EverythingWhentWell()
+
+
+results = []
+inputvalue_list = ''
+w_list = ''
+c = ''
+Main()
