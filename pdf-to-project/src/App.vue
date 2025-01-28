d00d156d6f90ce75574c07933d7732fe863ee1ef
Marijndh <marijn.den.hollander@student.nhlstenden.com>
2025-01-28 14:35:10 +0100

    Added components

--- pdf-to-project/src/App.vue
+++ pdf-to-project/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <h1>💖 Hello World!</h1>
+
   <p>Welcome to your Electron application.</p>
   <button @click="count++">
     Count is: {{ count }}
   </button>
+  <v-row></v-row>
 </template>
 
 <script setup lang="ts">
