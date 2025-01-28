a37ade6518b75b2e6183a3916b2f77975b979b8b
Marijndh <marijn.den.hollander@student.nhlstenden.com>
2025-01-28 14:26:58 +0100

    Added electron project

---
+++ pdf-to-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>💖 Hello World!</h1>
+  <p>Welcome to your Electron application.</p>
+  <button @click="count++">
+    Count is: {{ count }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {Ref} from "@vue/runtime-dom";
+
+const count: Ref<number, number> = ref(0);
+</script>
